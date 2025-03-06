99e420c3d250f69ba7b94071e42b43286e7d3fe0
Baguette42 <nevenlegall@gmail.com>
2025-03-06 12:18:38 +0100

    Add quest Deadline + Migration

--- src/quest/dto/create-quest.dto.ts
+++ src/quest/dto/create-quest.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import {IsDate, IsInt, IsNotEmpty, IsString, IsUrl} from "class-validator";
+import { IsDate, IsInt, IsNotEmpty, IsString, IsUrl } from "class-validator";
 
 export class CreateQuestDto {
   @ApiProperty()
