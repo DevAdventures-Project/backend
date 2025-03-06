ca455bf9e41e3544ba8cec5afd3eaeff61c7c3e1
DUPLESSIS Alexis <98809630+meizarkos@users.noreply.github.com>
2025-03-06 19:54:22 +0100

    lint

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
-import { ClassSerializerInterceptor, LogLevel, ValidationPipe } from "@nestjs/common";
+import {
+  ClassSerializerInterceptor,
+  LogLevel,
+  ValidationPipe,
+} from "@nestjs/common";
 import { NestFactory, Reflector } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-
   const allLogLevels: LogLevel[] = [
     "verbose",
     "debug",
